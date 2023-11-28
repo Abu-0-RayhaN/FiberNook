4bd820b39c072623dea9ea3b7d4b11e36565e0fc
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-11-28 20:19:16 +0600

    api setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useEffect, useState } from "react";
+// import axios from "axios";
+const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function FetchData() {
+      console.log(`${import.meta.env.VITE_API_URL}`);
+      try {
+        const reponse = await fetch(`${import.meta.env.VITE_API_URL}`);
+        if (!reponse.ok) {
+          throw new Error("Network reponse was not ok");
+        }
+        const result = await reponse.json();
+        console.log("results is", result);
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    }
+    FetchData();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {data} <p>hello world</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
