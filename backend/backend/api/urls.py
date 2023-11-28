c259e4bfdc3ba1ae8aab00adeab468b5ec90efd3
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-11-28 17:27:55 +0600

    init

---
+++ backend/backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from posts.api.urls import post_router
+
+router = DefaultRouter()
+router.registry.extend(post_router.registry)
+urlpatterns = [
+    path('', include(router.urls))
+]
