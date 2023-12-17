3a1c1eac407d0ae68272e6b9b28f5077ffc0856c
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-12-17 21:38:16 +0600

    url hiding

--- frontend/src/services/userAuthApi.js
+++ frontend/src/services/userAuthApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const userAuthApi = createApi({
   reducerPath: "userAuthApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/api/user/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (user) => ({
